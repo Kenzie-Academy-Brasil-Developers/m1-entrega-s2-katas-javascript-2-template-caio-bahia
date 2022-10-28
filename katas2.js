72a1643f668132df70f4d3e9f84a82a1476e7d00
Caio <caiob1305@gmail.com>
2022-10-28 17:14:02 -0300

    katas2

--- katas2.js
+++ katas2.js
@@ -1,1 +1,55 @@
+function add(x,y){
+ 
+    return x+y
+}
+
+// console.log(add(parseInt(prompt("Coloque o primeiro Numero")),parseInt(prompt("Coloque o segundo Numero"))))
+
+function multiply(x,y){
+
+    let multi = 0
+
+
+        for(let i=1; i <= y; i++){
+
+            multi = add(multi,x)
+
+        }
+
+     return multi
+}
+
+// console.log(multiply(parseInt(prompt("Coloque o primeiro Numero")),parseInt(prompt("Coloque o segundo Numero"))) 
+
+function power(x,y){
+
+    let expo = x
+    
+    for(let i = 1; i <= y-1 ; i++){
+
+        expo = multiply(expo,x)
+
+    }
+
+    return expo
+}
+
+// console.log(power(parseInt(prompt("Coloque o primeiro Numero")),parseInt(prompt("Coloque o segundo Numero"))))
+
+function factorial(x){
+
+    let fator = 1
+    
+
+    for(let i=x; i >= 1; i--){
+
+        fator = multiply(fator,i)
+    }
+    return fator
+}
+
+console.log(factorial(parseInt(prompt("Coloque um Numero"))))
+
+
+
 
